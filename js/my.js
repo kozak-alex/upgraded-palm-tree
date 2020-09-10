2fe63c30e1c5be37048eda07d0ee55ecc8d93f46
kozak-alex <kozachenko2578@gmail.com>
2020-09-10 18:49:46 +0300

    calculator

---
+++ js/my.js
@@ -0,0 +1,74 @@
+var mydict={
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4
+}
+calc.one.addEventListener('click', function() {
+    calc.result.value += mydict[one];
+}, false)
+
+calc.two.addEventListener('click', function() {
+    calc.result.value += '2';
+}, false)
+
+calc.three.addEventListener('click', function() {
+    calc.result.value += '3';
+}, false)
+
+calc.four.addEventListener('click', function() {
+    calc.result.value += '4';
+}, false)
+
+calc.five.addEventListener('click', function() {
+    calc.result.value += '5';
+}, false)
+
+calc.six.addEventListener('click', function() {
+    calc.result.value += '6';
+}, false)
+
+calc.seven.addEventListener('click', function() {
+    calc.result.value += '7';
+}, false)
+
+calc.eight.addEventListener('click', function() {
+    calc.result.value += '8';
+}, false)
+
+calc.nine.addEventListener('click', function() {
+    calc.result.value += '9';
+}, false)
+
+calc.zero.addEventListener('click', function() {
+    calc.result.value += '0';
+}, false)
+
+calc.point.addEventListener('click', function() {
+    calc.result.value += '.';
+}, false)
+/*---------------------------------------------*/
+calc.add.addEventListener('click', function(){
+    calc.result.value += '+';
+}, false)
+
+calc.sub.addEventListener('click', function(){
+    calc.result.value += '-';
+}, false)
+
+calc.mul.addEventListener('click', function(){
+    calc.result.value += '*';
+}, false)
+
+calc.div.addEventListener('click', function(){
+    calc.result.value += '/';
+}, false)
+/*---------------------------------------------*/
+calc.equal.addEventListener('click', function() {
+    calc.result.value = eval(calc.result.value);
+}, false)
+
+calc.clear.addEventListener('click', function() {
+    calc.result.value = null;
+}, false)
+
