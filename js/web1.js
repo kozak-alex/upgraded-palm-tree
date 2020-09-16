66bb74847c2f9841eeaff4b675559c48cbd65fee
kozak-alex <kozachenko2578@gmail.com>
2020-09-16 22:05:37 +0300

    Weater

--- js/web1.js
+++ js/web1.js
@@ -1,17 +1,13 @@
 
-let id_city;
-let city; 
-const kostil = "https://cors-anywhere.herokuapp.com";
-const realServer = "`https://www.metaweather.com/api/location/search/?query=";
-const realWeather = "https://www.metaweather.com/api/location/";
 
 $(() => {
-    let sel_city;
+    let sel_city;/* = ($('select').val()).toLowerCase();*/
     $('#GetID').on('click', () => {
         sel_city = ($('select').val()).toLowerCase();
         console.log(sel_city);
         recive_id_city(sel_city);
     });
+
 
     let id_city;
      $('#GetWeather').on('click', () => {
@@ -19,7 +15,9 @@ $(() => {
          console.log(id_city);
          recive_weather_city(id_city);
     });
+
 });
+
 
 function recive_id_city(city) {
     const kostil = "https://cors-anywhere.herokuapp.com";
@@ -57,3 +55,50 @@ function recive_weather_city(ident) {
 }
 
 
+
+let id_city;
+let city; 
+const kostil = "https://cors-anywhere.herokuapp.com";
+const realServer = "`https://www.metaweather.com/api/location/search/?query=";
+const realWeather = "https://www.metaweather.com/api/location/";
+
+
+/*
+ 
+ const realServer = "https://www.metaweather.com/api/location/44418/";
+ fetch(`${kostil}/${realServer}`)
+ .then(response => response.json())
+ .then(json => {
+    temp_day1.innerHTML = json.consolidated_weather[0].the_temp;
+    icon_day1.src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`
+    time.innerHTML = json.time;
+ })
+ .catch(err => {
+     console.error(err);
+ });
+
+
+*/
+
+
+
+
+/*
+const city = $(()=>{
+    const a=$('select').val();
+    return a;
+});*/
+/*
+let city; 
+const kostil = "https://cors-anywhere.herokuapp.com";
+const realServer = `https://www.metaweather.com/api/location/search/?query=${city}`;
+alert('123');
+fetch(`${kostil}/${realServer}`)
+.then(response => response.json())
+.then(json => {
+   const {woeid} = json[0];
+   identification.innerHTML = `Kiev id ${woeid}`;
+})
+.catch(err => console.error(err));
+
+*/
